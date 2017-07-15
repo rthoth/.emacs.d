1f4c4bfd0e84da30bc155be8cc5ffd18261f71aa
rthoth <ronaldo.asilva@gmail.com>
2017-07-15 17:26:45 -0300

    Changed ibuffer

--- init.el
+++ init.el
@@ -137,6 +137,8 @@
 
 (use-package adoc-mode)
 
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
