af74d9030c36edc88cd3a3b08bc4c0203b01d44e
Ronaldo <ronaldo.silva@lemaf.ufla.br>
2017-08-04 18:48:46 -0300

    Ensime.

--- init.el
+++ init.el
@@ -176,4 +176,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Hack")))))
+ '(default ((t (:family "Hack"))))
+ '(ensime-implicit-highlight ((t (:underline nil)))))
