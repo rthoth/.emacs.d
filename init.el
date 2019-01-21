9c474fe6a90e1bff7e528dc55b45541287f43ca0
Ronaldo <ronaldo.silva@lemaf.ufla.br>
2019-01-21 14:05:26 -0200

    projectile

--- init.el
+++ init.el
@@ -84,7 +84,11 @@
   :bind
   ("<f8>" . neotree-toggle))
 
-(use-package projectile)
+(use-package projectile
+  :config
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (projectile-mode +1))
 
 (use-package helm
   :config
